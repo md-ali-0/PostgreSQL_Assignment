87629b4489ab398fcfcd56c997e665ac27307d95
md-ali-0 <mohammad.98482@gmail.com>
2024-10-23 02:15:03 +0600

    Retrieve the names of students using a limit of 2, starting from the 3rd student.

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -78,3 +78,7 @@ UPDATE students
 -- Delete all courses that have no students enrolled.
 DELETE FROM courses 
     WHERE course_id NOT IN(SELECT DISTINCT course_id FROM enrollment);
+
+-- Query 5:
+-- Retrieve the names of students using a limit of 2, starting from the 3rd student.
+SELECT student_name FROM students LIMIT 2 OFFSET 2
