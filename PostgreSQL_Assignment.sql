97e859c98634eb2ce7695b0075500d360adf2098
md-ali-0 <mohammad.98482@gmail.com>
2024-10-23 02:44:12 +0600

    Retrieve the course names and the number of students enrolled in each course.

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -82,3 +82,9 @@ DELETE FROM courses
 -- Query 5:
 -- Retrieve the names of students using a limit of 2, starting from the 3rd student.
 SELECT student_name FROM students LIMIT 2 OFFSET 2
+
+-- Query 6:
+-- Retrieve the course names and the number of students enrolled in each course.
+SELECT c.course_name, COUNT(e.student_id) AS students_enrolled FROM courses c
+JOIN enrollment e ON e.course_id = c.course_id
+GROUP BY c.course_name
